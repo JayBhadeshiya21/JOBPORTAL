35620dfbd815e5d9a5ad354fd720842f36dd32cd
JayBhadeshiya21 <bhadeshiyajay773@gmail.com>
2026-02-18 19:51:26 +0530

    Job profile backend side setup

--- backend/controllers/jobController.js
+++ backend/controllers/jobController.js
@@ -7,6 +7,12 @@ const SavedJob = require("../models/SavedJob");
 // @desc  Create a new job (Employer only)
 exports.createJob = async(req, res) => {
     try {
+        if(req.User.role != 'employer') {
+            return res.status(403).json({ message: "Only employer can post jobs"})
+        }
+
+        const job = await Job.create({ ...req.body, company: req.user._id });
+        res.status(201).json(job);
 
     } catch (err) {
         res.status(500).json({ message: err.message});
@@ -14,7 +20,42 @@ exports.createJob = async(req, res) => {
 };
 
 exports.getJobs = async(req, res) => {
+
+    const {
+        keyword,
+        location,
+        category,
+        type,
+        minSalary,
+        maxSalary,
+        userId,
+    } = req.query;
+
+    const query = {
+        isClosed: false,
+        ...(keyword && { tittle : { $regex: keyword, $options: "i" }}),
+        ...(location && { location: { $regex: location, $options: "i"}}),
+        ...(category && { category}),
+        ...(type && { type}),
+    }
+
+    if( minSalary || maxSalary) {
+        query.$and = [];
+
+        if(minSalary) {
+            query.$and.push({ salaryMax: { $gte: Number(minSalary) }});
+        }
+
+        if(maxSalary) {
+            query.$and.push({ salaryMin: { $lte: Number(maxSalary) }});
+        }
+
+        if(query.$and.length === 0) {
+            delete query.$and;
+        }
+    }
     try {
+        
 
     } catch (err) {
         res.status(500).json({ message: err.message});
